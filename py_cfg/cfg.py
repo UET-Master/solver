59d2aef228bba8b877e5ca111beeb209792104c2
Kien Le <kienle371999@gmail.com>
2024-11-14 13:29:37 +0700

    Update the cfg file

--- py_cfg/cfg.py
+++ py_cfg/cfg.py
@@ -2,4 +2,4 @@ from py2cfg import CFGBuilder
 
 cfg = CFGBuilder().build_from_file('example', 'example.py')
 # cfg.build_visual('exampleCFG', 'dot')
-cfg.build_visual('exampleCFG', 'pdf')
+cfg.build_visual('example_cfg', 'pdf')
